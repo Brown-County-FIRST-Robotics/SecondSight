0a1a36cd6f2a3fe97510e7fdc37ae8abe1dc9bcd
Colin <cfinn68916@gmail.com>
2023-04-10 22:47:05 -0500

    Move to a new webserver system using multiple files and threading. Probably a terrible idea.
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/main.py
+++ SecondSight/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
 import sys
-import SecondSight.webserver.server
+import time
+
+import SecondSight.webserver.DEATHSTARE
+from flask import Flask
+import threading
+import asyncio
 
 def main_cli():
-    app = SecondSight.webserver.server.get_app()
-    app.run()
+    app = Flask(__name__)
+    SecondSight.webserver.DEATHSTARE.start(app)
+    threading.Thread(target=app.run,kwargs={'host': "0.0.0.0"}).start()
+    while True:
+        time.sleep(0.001)
 
 if __name__ == "__main__":
     # This file should never be run
