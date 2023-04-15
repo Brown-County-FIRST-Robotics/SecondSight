02758f9008c88605522b8c0c93b7362749e2b047
Josh Bressers <josh@bress.net>
2023-04-14 20:29:24 -0500

    Copy over the color detection module
    Signed-off-by: Josh Bressers <josh@bress.net>

--- SecondSight/webserver/DEATHSTARE.py
+++ SecondSight/webserver/DEATHSTARE.py
@@ -2,6 +2,7 @@
 
 import logging
 from flask import Flask, render_template, Response, redirect, request, jsonify
+import SecondSight.Color
 
 
 def start(app):
@@ -9,6 +10,14 @@ def start(app):
     def index():
         """The default page"""
         return render_template('index.html')
+
+    @app.route('/preview_image')
+    def preview_image():
+        logging.debug("DEATHSTARE.preview_image")
+        the_camera = app.cameras[0]
+        #Video streaming route. Put this in the src attribute of an img tag
+        return Response(SecondSight.Color.gen_preview_picker(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 
 if __name__ == "__main__":
