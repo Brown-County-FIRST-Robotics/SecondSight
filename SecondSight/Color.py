cd3c40b5c16e8051ac0deeb4ffb0c98c000db46b
Colin <cfinn68916@gmail.com>
2023-04-16 16:09:59 -0500

    Add cube detection
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/Color.py
+++ SecondSight/Color.py
@@ -33,32 +33,41 @@ def findObject(frame, cone_color, cube_color):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     cone_object_mask = cv2.inRange(hsv, cone_color[0], cone_color[1])
+    cube_object_mask = cv2.inRange(hsv, cube_color[0], cube_color[1])
 
     cone_res = cv2.bitwise_and(frame, cone_object_mask)
-    contours, _ = cv2.findContours(cv2.cvtColor(cone_res, cv2.COLOR_BGR2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cone_contours, _ = cv2.findContours(cv2.cvtColor(cone_res, cv2.COLOR_BGR2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # find largest contour
+    cube_res = cv2.bitwise_and(frame, cube_object_mask)
+    cube_contours, _ = cv2.findContours(cv2.cvtColor(cube_res, cv2.COLOR_BGR2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
     res = []
-    for contour in contours:
+    for contour in cone_contours:
         x, y, width, height, theta = cv2.minAreaRect(contour)
-        res.append(GamePiece(x, y, width, height, theta))
+        res.append(GamePiece(x, y, width, height, theta, True))
+    for contour in cube_contours:
+        x, y, width, height, theta = cv2.minAreaRect(contour)
+        res.append(GamePiece(x, y, width, height, theta, False))
     return res
 
 
 # GamePiece - can represent a cone or a cube
 class GamePiece:
-    def __init__(self, x, y, width, height, theta):
+    def __init__(self, x, y, width, height, theta, is_cone):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.theta = theta
-# fix this later
-#    def drawBoundRect(self, frame, color):
-#        return cv2.rectangle(frame, (int(self.imgX - self.w / 2), int(self.imgY - self.h / 2)),
-#                             (int(self.imgX + self.w / 2), int(self.imgY + self.h / 2)), color, 4, cv2.LINE_AA)
+        self.isCone = is_cone
 
-#Color picker
+    def drawBoundRect(self, frame, color):  # TODO: test
+        box = cv2.boxPoints((self.x, self.y, self.width, self.height, self.theta))
+        box = np.int0(box)
+        cv2.drawContours(frame, [box], 0, color, 2)
+
+
+# Color picker
 # This function gets called by the /video_feed route below
 def gen_preview_picker(camera):  # generate frame by frame from camera
     logging.debug("Vision.gen_frames_picker")
