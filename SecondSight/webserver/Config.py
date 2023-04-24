7590544890ad260c71a3e32a11f637cd7ef4d296
Colin <cfinn68916@gmail.com>
2023-04-24 12:09:38 -0500

    Add selector for universal game piece detection
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/webserver/Config.py
+++ SecondSight/webserver/Config.py
@@ -14,7 +14,10 @@ def start(app):
         else:
             conf.set_value('nt_dest', request.form['nt_addr'])
             conf.set_value('cameras', [])
+            conf.set_value('detects', [])
             for k, v in request.form.items():
+                if k == 'cube2023':
+                    conf.set_value('detects', conf.get_value('detects')+['cube2023'])
                 if k.startswith('cam_port_'):
                     roles = []
                     if f"apriltags_{k.split('_')[-1]}" in request.form:
@@ -33,7 +36,7 @@ def start(app):
             app.cameras = SecondSight.Cameras.loadCameras()
             conf.set_value('config_required', False)
             conf.write()
-            return redirect('/')
+            return Response('Please restart the code')
 
 
 if __name__ == "__main__":
