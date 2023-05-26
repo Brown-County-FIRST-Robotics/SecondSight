f717ed1c36f57e66fdf32abd03f138e390d4c770
Colin <cfinn68916@gmail.com>
2023-05-26 09:31:02 -0500

    Remove roll threshold becuase it is no longer the perferred method of eliminating false detections.
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/AprilTags/Detector.py
+++ SecondSight/AprilTags/Detector.py
@@ -111,7 +111,7 @@ def getCoords(img, valid_tags=range(1, 9), check_hamming=True):
     return detections
 
 
-def getPosition(img, camera_matrix, dist_coefficients, valid_tags=range(1, 9), roll_threshold=20, check_hamming=True):
+def getPosition(img, camera_matrix, dist_coefficients, valid_tags=range(1, 9), check_hamming=True):
     """
     This function takes an image and returns the position of apriltags in the image
 
@@ -119,7 +119,6 @@ def getPosition(img, camera_matrix, dist_coefficients, valid_tags=range(1, 9), r
     :param camera_matrix: The camera's calibration matrix
     :param dist_coefficients: The distortion coefficients of the camera
     :param valid_tags: (Default: 1-9) The apriltags to look for
-    :param roll_threshold: (Default: 0.3 radians/17 degrees) The maximum roll of the apriltag. Helps remove false detections.
     :param check_hamming: (Default: True) Checks if the hamming value is 0
     :return: A list of Detection objects, or None if it fails
     :rtype: list(Detection objects), or None if no apriltags are found
@@ -156,10 +155,6 @@ def getPosition(img, camera_matrix, dist_coefficients, valid_tags=range(1, 9), r
             up_down = -translation_vector[0][1] * 2.54
             distance = translation_vector[0][2] * 2.54
 
-            # Check if roll is within limit
-            if math.fabs(roll) > roll_threshold:
-                logging.info(f'discarded a value (roll:{roll})')
-                continue
             logging.info(f'april pos: yaw:{str(yaw)[:5]}, lr:{str(left_right)[:7]}, distance:{str(distance)[:7]}, rms:{rms}, tag:{tagid}')
             detections.append(Detection(yaw, pitch, roll, left_right[0], up_down[0], distance[0], rms[0][0], tagid))
     return detections
