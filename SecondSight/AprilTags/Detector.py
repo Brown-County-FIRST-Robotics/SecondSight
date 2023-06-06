5b8e882e0e4a0af8a644907fb3b4728dd706fa80
Colin <cfinn68916@gmail.com>
2023-06-06 10:38:43 -0500

    Use fuseApriltags() to send one field value over networktables
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/AprilTags/Detector.py
+++ SecondSight/AprilTags/Detector.py
@@ -200,11 +200,13 @@ class ApriltagManager:
 
     def __init__(self):
         self.current_apriltags: List[Detection] = []
+        self.current_field_pos: tuple[float, float, float] = (-1, -1, -1)
         self.april_table = networktables.NetworkTables.getTable('SecondSight').getSubTable('Apriltags')
         self.fetchApriltags()
 
     def fetchApriltags(self):
-        res = []
+        json_res = []
+        res: List[Detection] = []
         cams = SecondSight.Cameras.CameraManager.getCameras()
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=len(cams))
         futures = {}
@@ -214,11 +216,17 @@ class ApriltagManager:
         for i, future in futures.items():
             dets = future.result()
             if dets:
+                res += dets
                 for det in dets:
                     det = det.json(error=True)
                     det['camera'] = i
-                    res.append(det)
-        self.current_apriltags = res
+                    json_res.append(det)
+        self.current_apriltags = json_res
+        if len(res) == 1:
+            res[0].calcFieldPos('2023')  # TODO: use the config to get the year
+            self.current_field_pos = (res[0].field_x, res[0].field_y, res[0].field_yaw)
+        elif len(res) > 1:
+            self.current_field_pos = fuseApriltags(res, '2023')  # TODO: use the config to get the year
 
     def getApriltags(self):
         return self.current_apriltags
@@ -233,6 +241,9 @@ class ApriltagManager:
                             det['distance_std'], det['left_right_std'], det['yaw_std'], det['rms'], det['error'],
                             det['tagid'], det['camera']]
             self.april_table.putNumberArray('relative_positions', nt_send)
+            self.april_table.putNumber('field_x', self.current_field_pos[0])
+            self.april_table.putNumber('field_y', self.current_field_pos[1])
+            self.april_table.putNumber('field_ang', self.current_field_pos[2])
 
 
 if __name__ == "main":
