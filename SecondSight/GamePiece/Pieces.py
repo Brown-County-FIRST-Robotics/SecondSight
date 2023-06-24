c7b5fa30f9cf52e94153e6243e9a8b5aa16db713
Colin <cfinn68916@gmail.com>
2023-06-24 13:50:49 -0500

    Add jsonify method to GamePiece class
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/GamePiece/Pieces.py
+++ SecondSight/GamePiece/Pieces.py
@@ -36,6 +36,20 @@ class BaseGamePiece:
         self.up_down = translation_vector[0][1]
         self.distance = translation_vector[0][2]
 
+    def jsonify(self, error=False):
+        res = {
+            'pitch': self.pitch,
+            'yaw': self.yaw,
+            'roll': self.roll,
+            'left_right': self.left_right,
+            'up_down': self.up_down,
+            'distance': self.distance,
+            'type': self.piece_type
+        }
+        if error:
+            res['rms'] = self.rms
+        return res
+
     def draw(self, frame, color: tuple[int, int, int], thickness: int = 2):  # TODO: test
         box = cv2.boxPoints((self.x, self.y), (self.width, self.height), self.theta)
         box = np.int0(box)
