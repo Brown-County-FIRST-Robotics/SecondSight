741c2cc68a0bb85c808444044ea2693c4a8342de
Colin <cfinn68916@gmail.com>
2023-10-07 00:14:35 -0500

    Add funtion for getting the date in ISO 8601 format
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/utils.py
+++ SecondSight/utils.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 
 def LogMe(func):
@@ -16,3 +17,8 @@ def LogMe(func):
         logging.debug(msg)
         return fn_out
     return inner
+
+
+def get8601date():
+    now = time.localtime()
+    return f'{now.tm_year}{now.tm_mon}{now.tm_mday if now.tm_mday > 9 else "0" + str(now.tm_mday)}{now.tm_hour if now.tm_hour > 9 else "0" + str(now.tm_hour)}{now.tm_min if now.tm_min > 9 else "0" + str(now.tm_min)}{now.tm_sec if now.tm_sec > 9 else "0" + str(now.tm_sec)}'
