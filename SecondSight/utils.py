7d63a6f765b102a37a490d8819f424adaac8eac0
Colin <cfinn68916@gmail.com>
2023-10-09 17:58:57 -0500

    Add utils.waitForNT()
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/utils.py
+++ SecondSight/utils.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import ntcore
 
 
 def LogMe(func):
@@ -22,3 +23,27 @@ def LogMe(func):
 def get8601date():
     now = time.localtime()
     return f'{now.tm_year}{now.tm_mon}{now.tm_mday if now.tm_mday > 9 else "0" + str(now.tm_mday)}{now.tm_hour if now.tm_hour > 9 else "0" + str(now.tm_hour)}{now.tm_min if now.tm_min > 9 else "0" + str(now.tm_min)}{now.tm_sec if now.tm_sec > 9 else "0" + str(now.tm_sec)}'
+
+
+def waitForNT() -> bool:
+    inst = ntcore.NetworkTableInstance.getDefault()
+    st = time.time()
+    while time.time() - st < 10:
+        if inst.isConnected():
+            break
+        time.sleep(0.1)
+    else:
+        return False
+    table = inst.getTable('FMSInfo')
+    st = time.time()
+    while time.time() - st < 10:
+        if table.getEntry('FMSControlData').getInteger(None) is not None and \
+                table.getString('EventName', None) is not None and \
+                table.getEntry('MatchType').getInteger(None) is not None and \
+                table.getEntry('ReplayNumber').getInteger(None) is not None and \
+                table.getEntry('MatchNumber').getInteger(None) is not None:
+            break
+        time.sleep(0.1)
+    else:
+        return False
+    return True
