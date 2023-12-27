ef0f37f92a6261ab824330f51c8c536f14627ac6
Colin <cfinn68916@gmail.com>
2023-12-27 00:22:43 -0600

    Move recordings to seperate directory
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/Recorder.py
+++ SecondSight/Recorder.py
@@ -31,9 +31,9 @@ class RecordingManager:
         rtable = inst.getTable(SecondSight.config.Configuration().get_value('inst_name'))
         self.publishers = [(rtable.getSubTable(str(i)).getBooleanTopic("isRecording").publish(), rtable.getSubTable(str(i)).getStringTopic("recordingPath").publish()) for i in range(len(SecondSight.Cameras.CameraManager.getCameras()))]
         for i, cam in enumerate(SecondSight.Cameras.CameraManager.getCameras()):
-            self.writers[i] = cv2.VideoWriter(f'{name}_{i}.avi', cv2.VideoWriter_fourcc(*'MP42'), 15.0, (int(cam.width), int(cam.height)))
+            self.writers[i] = cv2.VideoWriter(f'recordings/{name}_{i}.avi', cv2.VideoWriter_fourcc(*'MP42'), 15.0, (int(cam.width), int(cam.height)))
             self.publishers[i][0].set(True)
-            self.publishers[i][1].set(f'{name}_{i}.avi')
+            self.publishers[i][1].set(f'recordings/{name}_{i}.avi')
 
     def loop(self):
         if self.isRecording:
