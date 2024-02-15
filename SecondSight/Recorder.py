b8a213938870f20688750e3305987a83da802008
Colin <cfinn68916@gmail.com>
2024-02-15 16:17:25 -0600

    Enforce 10hz framerate in recording
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- SecondSight/Recorder.py
+++ SecondSight/Recorder.py
@@ -1,3 +1,5 @@
+import random
+import time
 from typing import Tuple, List
 
 import ntcore
@@ -18,6 +20,7 @@ class RecordingManager:
     def __init__(self):
         self.isRecording = False
         self.rebuild()
+        self.lastTime = 0
 
     def rebuild(self):
         self.stopRecording()
@@ -28,8 +31,7 @@ class RecordingManager:
         if self.isRecording:
             self.startRecording()
 
-
-    def startRecording(self, name: str = SecondSight.utils.get8601date()):
+    def startRecording(self, name: str = SecondSight.utils.get8601date()+str(random.randint(0,100000))):
         if self.isRecording:
             return
         self.isRecording = True
@@ -38,14 +40,15 @@ class RecordingManager:
         rtable = inst.getTable(SecondSight.config.Configuration().get_value('inst_name'))
         self.publishers = [(rtable.getSubTable(str(i)).getBooleanTopic("isRecording").publish(), rtable.getSubTable(str(i)).getStringTopic("recordingPath").publish()) for i in range(len(SecondSight.Cameras.CameraManager.getCameras()))]
         for i, cam in enumerate(SecondSight.Cameras.CameraManager.getCameras()):
-            self.writers[i] = cv2.VideoWriter(f'recordings/{name}_{i}.avi', cv2.VideoWriter_fourcc(*'MP42'), 15.0, (int(cam.width), int(cam.height)))
+            self.writers[i] = cv2.VideoWriter(f'recordings/{name}_{i}.avi', cv2.VideoWriter_fourcc(*'MP42'), 10.0, (int(cam.width), int(cam.height)))
             self.publishers[i][0].set(True)
             self.publishers[i][1].set(f'recordings/{name}_{i}.avi')
 
     def loop(self):
         if len(self.writers) != len(SecondSight.Cameras.CameraManager.getCameras()):
             self.rebuild()
-        if self.isRecording:
+        if self.isRecording and time.time()-self.lastTime>0.1:
+            self.lastTime=time.time()
             for cam, writer in zip(SecondSight.Cameras.CameraManager.getCameras(), self.writers):
                 writer.write(cam.frame)
 
